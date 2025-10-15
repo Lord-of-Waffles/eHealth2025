b627900416ace3168eb8a4a4e6c4d24f56a297d8
Lord-of-Waffles <ben.worton@gmail.com>
2025-10-15 15:26:20 +0200

    write initial get_data function

--- modules/get_data.py
+++ modules/get_data.py
@@ -3,4 +3,16 @@ import numpy as np
 
 # This file reads data from flat files (CSV)
 def get_data():
-    return None
+
+    # get all data from .csv files, allocate to separate dataframes
+
+    df_clinical_train = pd.DataFrame(pd.read_csv("./data_hn/data_hn_clinical_train.csv"))
+    df_ct_train = pd.DataFrame(pd.read_csv("./data_hn/data_hn_ct_train.csv"))
+    df_pt_train = pd.DataFrame(pd.read_csv("./data_hn/data_hn_pt_train.csv"))
+
+    # return dictionary of these dataframes
+    return {
+        "clinical" : df_clinical_train,
+        "ct" : df_ct_train,
+        "pt" : df_pt_train
+    }
