bb48327a7d11bd8b38d6aaff17b05eb5c3a1554c
Lord-of-Waffles <ben.worton@gmail.com>
2025-10-19 11:40:47 +0200

    Fixed imports in main.py

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import pandas as pd
 import numpy as np
-import modules.clean as clean
-import modules.get_data as get_data
+from modules.clean import clean
+from modules.get_data import get_data
 
 
 def main():
     # Do we want to use all the .csv files? currently get_data returns a dict with dataframes from all 3 files. - Ben
     df_dict = get_data()
     clean(df_dict)
+
     
 
 
