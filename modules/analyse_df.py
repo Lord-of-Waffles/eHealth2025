2dac257564ab5f3d5b4a8c74a65684e270aee48c
Lord-of-Waffles <ben.worton@gmail.com>
2025-10-19 11:57:11 +0200

    Formatted printing

--- modules/analyse_df.py
+++ modules/analyse_df.py
@@ -1,13 +1,14 @@
 import pandas as pd
 import numpy as np
 
+# Takes a dataframe as input, runs some simple functions to see the top & bottom rows + a statistic summary - Ben
 def analyse_df(df):
-    print("Analysing Dataframe:")
-    print("1. df.head()")
+    print("\nAnalysing Dataframe:")
+    print("\n1. df.head()")
     print(df.head())
-    print("<==================>")
+    print("\n<====================================================================================================================>")
     print("2. df.tail()")
     print(df.tail())
-    print("<==================>")
+    print("\n<====================================================================================================================>")
     print("3. df.describe()")
     print(df.describe())
