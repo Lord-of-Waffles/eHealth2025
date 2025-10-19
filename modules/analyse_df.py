f7dfd5b5ec856f858d20be8185170ed1f6614ddf
Lord-of-Waffles <ben.worton@gmail.com>
2025-10-19 14:01:25 +0200

    Add checking for duplicates in analyse_df.py

--- modules/analyse_df.py
+++ modules/analyse_df.py
@@ -3,6 +3,7 @@ import numpy as np
 
 # Takes a dataframe as input, runs some simple functions to see the top & bottom rows + a statistic summary - Ben
 def analyse_df(df):
+    duplicate_sum = df.duplicated().sum()
     print("\nAnalysing Dataframe:")
     print("\n1. df.head()")
     print(df.head())
@@ -12,3 +13,10 @@ def analyse_df(df):
     print("\n<====================================================================================================================>")
     print("3. df.describe()")
     print(df.describe())
+    print("\n<====================================================================================================================>")
+    print("4. How many duplicates:")
+    print(f"Duplicate row count: {duplicate_sum}")
+    # show which rows are duplicated if there are any - Ben
+    if duplicate_sum > 0:
+        print("These are rows are duplicates:")
+        print(df.duplicated())
