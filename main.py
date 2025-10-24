fa6415ef59f7a61aafb98dbd77183d8440377411
Lord-of-Waffles <ben.worton@gmail.com>
2025-10-24 11:42:45 +0200

    added training data handling, model is able to run on clinical data

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ import pandas as pd
 import numpy as np
 from modules.clean import clean
 from modules.get_data import get_data
+from modules.get_test_data import get_test_data
 from modules.analyse_df import analyse_df
 from modules.fix_missing_values_for_clinical import fix_missing_values
 from modules.train_model import train_model
+from modules.test_model import test_model
 
 
 def main():
@@ -12,9 +14,18 @@ def main():
     df_dict = get_data()
     #clean(df_dict)
     df_dict["clinical"] = fix_missing_values(df_dict["clinical"])
-    print("Starting Training")
+
+    print("Starting training...")
     trained_model = train_model(df_dict["clinical"])
-    print("Training complete!")
+    print("Training complete!\n")
+
+    print("Cleaning test data...")
+    df_test_dict = get_test_data()
+    df_test_dict["clinical"] = fix_missing_values(df_test_dict["clinical"])
+    print("Clinical test data cleaned!\n")
+
+    print("Starting test...")
+    print(test_model(df_test_dict["clinical"], trained_model))
 
     # can use these calls to take a look at the data from the dataframes :) - Ben
     #analyse_df(df_dict["clinical"]) # This has some missing data if not cleaned, there are missing values in Tobacco (10.1%), Alcohol (10%), Performance Status (9.3%) - Ben
