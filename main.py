5733facb24cea9f1b650b559c39a590b1eae1e5c
Lord-of-Waffles <ben.worton@gmail.com>
2025-11-04 13:53:10 +0100

    Created run functions for kNN

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from modules.analyse_df import analyse_df
 from modules.fix_missing_values_for_clinical import fix_missing_values
 from modules.train_model import train_model, train_model_ct, train_model_pt
 from modules.test_model import test_model, test_model_ct_pt
+from modules.train_model_knn import train_model_knn, train_model_ct_knn, train_model_pt_knn
 
 
 def run_clinical(df_train, df_test):
@@ -37,6 +38,35 @@ def run_pt(df_train, df_test, include_center_id=False):
     print("\n=== PET: Testing ===")
     test_model_ct_pt(df_test, clf_pt)
 
+def run_clinical_knn(df_train, df_test):
+    print("\n=== Clinical: Training (kNN) ===")
+    df_train = fix_missing_values(df_train.copy())
+    clf = train_model_knn(df_train)
+    print("Clinical training complete.")
+
+    print("\n=== Clinical: Testing ===")
+    df_test = fix_missing_values(df_test.copy())
+    test_model(df_test, clf)   # prints Accuracy, Report, Confusion Matrix
+
+
+def run_ct_knn(df_train, df_test, include_center_id=False):
+    print("\n=== CT: Training ===")
+    clf_ct = train_model_ct_knn(df_train, include_center_id=include_center_id)
+    print("CT training complete.")
+
+    print("\n=== CT: Testing ===")
+    test_model_ct_pt(df_test, clf_ct)  # prints Accuracy, ROC-AUC, Report, Confusion Matrix
+
+
+def run_pt_knn(df_train, df_test, include_center_id=False):
+    print("\n=== PET: Training ===")
+    clf_pt = train_model_pt_knn(df_train, include_center_id=include_center_id)
+    print("PET training complete.")
+
+    print("\n=== PET: Testing ===")
+    test_model_ct_pt(df_test, clf_pt)
+
+
 
 def main():
     df_dict = get_data()         
@@ -44,7 +74,14 @@ def main():
 
     #run_clinical(df_dict["clinical"], df_test_dict["clinical"])
     #run_ct(df_dict["ct"], df_test_dict["ct"], include_center_id=False)
-    run_pt(df_dict["pt"], df_test_dict["pt"], include_center_id=False)
+    #run_pt(df_dict["pt"], df_test_dict["pt"], include_center_id=False)
+
+
+    run_clinical_knn(df_dict["clinical"], df_test_dict["clinical"])
+    #run_ct_knn(df_dict["ct"], df_test_dict["ct"], include_center_id=False)
+    #run_pt_knn(df_dict["pt"], df_test_dict["pt"], include_center_id=False)
+
+
 
     # can use these calls to take a look at the data from the dataframes :) - Ben
     #analyse_df(df_dict["clinical"]) # This has some missing data if not cleaned, there are missing values in Tobacco (10.1%), Alcohol (10%), Performance Status (9.3%) - Ben
